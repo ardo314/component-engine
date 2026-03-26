692ab4af2fae22707bb2bee70f17c4c94802f2de
ardo314 <lhansing92@gmail.com>
2026-03-26 22:24:19 +0000

    changes

--- engine/core/src/component.ts
+++ engine/core/src/component.ts
@@ -1,20 +1,30 @@
 type Id = string & { readonly __brand: unique symbol };
 
-interface ComponentContract {
-  properties: Record<string, any>;
-}
+type ComponentContract = {
+  readonly properties: Record<string, any>;
+};
 
 interface Component {
-  id: Id;
-  contract: ComponentContract;
+  readonly id: Id;
+  readonly contract: ComponentContract;
 }
 
-export function defineComponent(
+export function defineComponent<const C extends ComponentContract>(
   id: Id,
-  contract: ComponentContract,
-): Component {
+  contract: C,
+): { readonly id: Id; readonly contract: C } {
   return {
     id: id,
     contract: contract,
   };
 }
+
+const exampleComponent = defineComponent("example" as Id, {
+  properties: {
+    name: "ExampleComponent",
+    value: 42,
+  },
+});
+
+export type ComponentProxy<T extends Component> = T["contract"]["properties"];
+type ExampleComponent = ComponentProxy<typeof exampleComponent>;
