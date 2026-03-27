56cd3c4681a04e47e109f0df39915ab37108ea7e
ardo314 <lhansing92@gmail.com>
2026-03-27 15:27:32 +0000

    changes

--- engine/core/src/index.ts
+++ engine/core/src/index.ts
@@ -1,6 +1,3 @@
 export { EntityId } from "./entity-id.js";
 export { defineComponent } from "./component.js";
-export type {
-  ComponentSchema as ComponentContract,
-  ComponentProxy,
-} from "./component.js";
+export type { ComponentProxy } from "./component.js";
