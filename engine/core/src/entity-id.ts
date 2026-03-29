e4d14002518075e6db58fcb34c0c2d07b8579f91
ardo314 <lhansing92@gmail.com>
2026-03-29 15:14:01 +0000

    changes

--- engine/core/src/entity-id.ts
+++ engine/core/src/entity-id.ts
@@ -1,8 +1,5 @@
 import z from "zod";
 
-/**
- * A serializable unique identifier for an entity.
- */
-export type EntityId = string & { readonly __brand: unique symbol };
+export const entityIdSchema = z.string().brand("EntityId");
 
-export const entityIdSchema = z.string() as unknown as z.ZodType<EntityId>;
+export type EntityId = z.infer<typeof entityIdSchema>;
