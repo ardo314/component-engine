916c7b6f08055bfafcdae8118ed47d39ed90158b
ardo314 <lhansing92@gmail.com>
2026-03-29 19:19:29 +0000

    changes

--- modules/core/src/index.ts
+++ modules/core/src/index.ts
@@ -1,5 +1,30 @@
 import { z } from "zod";
 
+export const vector2Schema = z.tuple([z.number(), z.number()]);
+
+export type Vector2 = z.infer<typeof vector2Schema>;
+
+export const vector3Schema = z.tuple([z.number(), z.number(), z.number()]);
+
+export type Vector3 = z.infer<typeof vector3Schema>;
+
+export const rotationVectorSchema = z.tuple([
+  z.number(),
+  z.number(),
+  z.number(),
+]);
+
+export type RotationVector = z.infer<typeof rotationVectorSchema>;
+
+export const quaternionSchema = z.tuple([
+  z.number(),
+  z.number(),
+  z.number(),
+  z.number(),
+]);
+
+export type Quaternion = z.infer<typeof quaternionSchema>;
+
 export const poseSchema = z.tuple([
   z.number(),
   z.number(),
