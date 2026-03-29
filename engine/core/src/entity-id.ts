b1be26ae2248dad2d18c752a15b84b8df3233aeb
ardo314 <lhansing92@gmail.com>
2026-03-29 12:41:49 +0000

    changes

--- engine/core/src/entity-id.ts
+++ engine/core/src/entity-id.ts
@@ -1,4 +1,8 @@
+import z from "zod";
+
 /**
  * A serializable unique identifier for an entity.
  */
 export type EntityId = string & { readonly __brand: unique symbol };
+
+export const entityIdSchema = z.string() as unknown as z.ZodType<EntityId>;
--- modules/nova/src/components.ts
+++ modules/nova/src/components.ts
@@ -1,14 +1,21 @@
-import { defineComponent } from "@engine/core";
+import { ComponentProxy, defineComponent, EntityId } from "@engine/core";
+import { entityIdSchema } from "@engine/core/src/entity-id.js";
 import { z } from "zod";
 
-export const Name = defineComponent("nova.name", {
+export const name = defineComponent("nova.name", {
   properties: {
     value: z.string(),
   },
 });
 
-export const Parent = defineComponent("nova.parent", {
+export type Name = typeof name;
+export type NameProxy = ComponentProxy<Name>;
+
+export const parent = defineComponent("nova.parent", {
   properties: {
-    value: z.string(),
+    value: entityIdSchema,
   },
 });
+
+export type Parent = typeof parent;
+export type ParentProxy = ComponentProxy<Parent>;
