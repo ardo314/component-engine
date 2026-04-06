6f09b9be55635359f0350018b17eb15c5950571b
ardo314 <lhansing92@gmail.com>
2026-04-06 10:35:38 +0000

    changes

--- modules/nova-workers/src/index.ts
+++ modules/nova-workers/src/index.ts
@@ -3,7 +3,6 @@ import { WorkerHost } from "@engine/module";
 import { NameWorker } from "./name-worker.js";
 import { ParentWorker } from "./parent-worker.js";
 import { PoseWorker } from "./pose-worker.js";
-import { FollowPoseWorker } from "./follow-pose-worker.js";
 
 const nc = await connect();
 const host = new WorkerHost(nc);
@@ -11,7 +10,6 @@ const host = new WorkerHost(nc);
 host.registerWorker(NameWorker);
 host.registerWorker(ParentWorker);
 host.registerWorker(PoseWorker);
-host.registerWorker(FollowPoseWorker);
 
 await host.listen();
 
