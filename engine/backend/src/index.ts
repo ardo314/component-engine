4f2ce60f63150e4939b6d994a3a3c2f42cbc9f44
ardo314 <lhansing92@gmail.com>
2026-04-12 09:58:22 +0000

    add user pass

--- engine/backend/src/index.ts
+++ engine/backend/src/index.ts
@@ -2,7 +2,11 @@ import { connect } from "nats";
 import { EntityHandler } from "./entity-handler.js";
 export { EntityRepository } from "./entity-repository.js";
 
-const nc = await connect({ servers: process.env.NATS_URL });
+const nc = await connect({
+  servers: process.env.NATS_URL,
+  user: process.env.NATS_USER,
+  pass: process.env.NATS_PASS,
+});
 const handler = new EntityHandler(nc);
 
 await handler.listen();
